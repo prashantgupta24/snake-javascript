d50db0844f85cc11767028837ca4c62c45235cb8
Prashant Gupta <prashantgupta@us.ibm.com>
2017-07-28 15:51:06 -0700

    finalizing

--- main.js
+++ main.js
@@ -2,7 +2,8 @@ $(function() {
 
   let snakeGame = function(snake) {
 
-    let numSegments = 20;
+    // the snake is divided into small segments, which are drawn and edited on each 'draw' call
+    let numSegments = 10;
     let direction = 'right';
     let playing = true;
 
@@ -51,6 +52,17 @@ $(function() {
       }
     };
 
+    /*
+     The snake segments are updated based on the direction of the snake.
+     All segments from 0 to n-1 are just copied over to 1 till n, i.e. segment 0
+     gets the value of segment 1, segment 1 gets the value of segment 2, and so on,
+     and this results in the movement of the snake.
+
+     The last segment is added based on the direction in which the snake is going,
+     if it's going left or right, the last segment's x coordinate is increased by a
+     predefined value 'diff' than its second to last segment. And if it's going up
+     or down, the segment's y coordinate is affected.
+    */
     function updateSnakeCoordinates() {
 
       for (let i = 0; i < numSegments - 1; i++) {
@@ -77,6 +89,11 @@ $(function() {
       }
     }
 
+    /*
+     I always check the snake's head position xCor[xCor.length - 1] and
+     yCor[yCor.length - 1] to see if it touches the game's boundaries
+     or if the snake hits itself.
+    */
     function checkGameStatus() {
       if (xCor[xCor.length - 1] > snake.width ||
           xCor[xCor.length - 1] < 0 ||
@@ -89,22 +106,16 @@ $(function() {
       }
     }
 
+    /*
+     If the snake hits itself, that means the snake head's (x,y) coordinate
+     has to be the same as one of its own segment's (x,y) coordinate.
+    */
     function checkSnakeCollision () {
-      // let indexOfX = xCor.indexOf(xCor[xCor.length - 1]);
-      // let indexOfY = yCor.indexOf(yCor[yCor.length - 1]);
-      //  console.log('xCor ->' + xCor);
-      //  console.log('yCor ->' + yCor);
-      // console.log(indexOfX, indexOfY);
-      // if( indexOfX == indexOfY) {
-      //   //console.log('xCor : ' + xCor.indexOf(xCor[xCor.length - 1]) + ', yCor : ' + yCor.indexOf(yCor[yCor.length - 1]));
-      //   console.log(indexOfX);
-      //   console.log(indexOfY);
-      // }
-      let lastX = xCor[xCor.length - 1];
-      let lastY = yCor[yCor.length - 1];
+      let snakeHeadX = xCor[xCor.length - 1];
+      let snakeHeadY = yCor[yCor.length - 1];
       for(let i=0;i<xCor.length-1;i++){
-        if(xCor[i] === lastX) {
-          if(yCor[i] === lastY) {
+        if(xCor[i] === snakeHeadX) {
+          if(yCor[i] === snakeHeadY) {
             //console.log('Hit!');
             return true;
           }
@@ -112,6 +123,11 @@ $(function() {
       }
     }
 
+    /*
+     Whenever the snake consumes a fruit, I increment the number of segments,
+     and just insert the tail segment again at the start of the array (basically
+     I add the last segment again at the tail, thereby extending the tail)
+    */
     function checkForFruit() {
       snake.point(xFruit, yFruit);
       if (xCor[xCor.length - 1] === xFruit && yCor[yCor.length - 1] === yFruit) {
