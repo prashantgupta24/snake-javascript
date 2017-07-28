ba823c2667d03ba3721477aa9cd9fcda8d448d25
Prashant Gupta <prashantgupta@us.ibm.com>
2017-07-27 21:18:05 -0700

    refactoring

--- main.js
+++ main.js
@@ -1,7 +1,6 @@
-
 $(function() {
 
-  let snakeGame = function (snake) {
+  let snakeGame = function(snake) {
 
     let numSegments = 5;
     let direction = 'right';
@@ -18,7 +17,7 @@ $(function() {
     let yFruit = 0;
     let score = $('#score');
 
-    function initialize () {
+    function initialize() {
       let canvas = snake.createCanvas(500, 500);
       canvas.parent('snakeCanvas');
       snake.frameRate(15);
@@ -27,49 +26,53 @@ $(function() {
       score.html(0);
       updateFruitCoordinates();
 
-      for(let i=0;i<numSegments;i++) {
-        xCor.push(xStart+(i*diff));
+      for (let i = 0; i < numSegments; i++) {
+        xCor.push(xStart + (i * diff));
         yCor.push(yStart);
       }
     }
 
-    function updateSnakeCoordinates () {
+    function updateSnakeCoordinates() {
 
-      for(let i=0;i<numSegments-1;i++) {
-        xCor[i]=xCor[i+1];
-        yCor[i]=yCor[i+1];
+      for (let i = 0; i < numSegments - 1; i++) {
+        xCor[i] = xCor[i + 1];
+        yCor[i] = yCor[i + 1];
       }
-      switch(direction){
-      case 'right' : xCor[numSegments-1] = xCor[numSegments-2] + diff;
-        yCor[numSegments-1] = yCor[numSegments-2];
+      switch (direction) {
+      case 'right':
+        xCor[numSegments - 1] = xCor[numSegments - 2] + diff;
+        yCor[numSegments - 1] = yCor[numSegments - 2];
         break;
-      case 'up' : xCor[numSegments-1] = xCor[numSegments-2];
-        yCor[numSegments-1] = yCor[numSegments-2] - diff;
+      case 'up':
+        xCor[numSegments - 1] = xCor[numSegments - 2];
+        yCor[numSegments - 1] = yCor[numSegments - 2] - diff;
         break;
-      case 'left' : xCor[numSegments-1] = xCor[numSegments-2] - diff;
-        yCor[numSegments-1] = yCor[numSegments-2];
+      case 'left':
+        xCor[numSegments - 1] = xCor[numSegments - 2] - diff;
+        yCor[numSegments - 1] = yCor[numSegments - 2];
         break;
-      case 'down' : xCor[numSegments-1] = xCor[numSegments-2];
-        yCor[numSegments-1] = yCor[numSegments-2] + diff;
+      case 'down':
+        xCor[numSegments - 1] = xCor[numSegments - 2];
+        yCor[numSegments - 1] = yCor[numSegments - 2] + diff;
         break;
       }
     }
 
-    function checkGameStatus () {
-      if(xCor[xCor.length-1] > snake.width ||
-         xCor[xCor.length-1] < 0 ||
-         yCor[yCor.length-1] > snake.height ||
-         yCor[yCor.length-1] < 0 ) {
+    function checkGameStatus() {
+      if (xCor[xCor.length - 1] > snake.width ||
+        xCor[xCor.length - 1] < 0 ||
+        yCor[yCor.length - 1] > snake.height ||
+        yCor[yCor.length - 1] < 0) {
         alert('Game ended! Your score was : ' + score.html());
         playing = false;
       }
     }
 
-    function checkForFruit () {
+    function checkForFruit() {
       snake.point(xFruit, yFruit);
-      if(xCor[xCor.length-1] === xFruit && yCor[yCor.length-1] === yFruit) {
+      if (xCor[xCor.length - 1] === xFruit && yCor[yCor.length - 1] === yFruit) {
         let prevScore = parseInt(score.html());
-        score.html((prevScore+1));
+        score.html((prevScore + 1));
         xCor.unshift(xCor[0]);
         yCor.unshift(yCor[0]);
         numSegments++;
@@ -77,30 +80,30 @@ $(function() {
       }
     }
 
-    function updateFruitCoordinates () {
+    function updateFruitCoordinates() {
       /*
         The complex math logic is because I wanted the point to lie
         in between 100 and width-100, and be rounded off to the nearest
         number divisible by 10, since I move the snake in multiples of 10.
       */
-      xFruit = (Math.floor((Math.random()*((snake.width-200)/10))+10)*10);
-      yFruit = (Math.floor((Math.random()*((snake.height-200)/10))+10)*10);
+      xFruit = (Math.floor((Math.random() * ((snake.width - 200) / 10)) + 10) * 10);
+      yFruit = (Math.floor((Math.random() * ((snake.height - 200) / 10)) + 10) * 10);
       //console.log("x - " + xFruit);
       //console.log("y - " + yFruit);
     }
 
-    snake.setup = function () {
+    snake.setup = function() {
       console.log('started');
       initialize();
     };
 
-    snake.draw = function () {
+    snake.draw = function() {
       // console.log('xCOR : ' + xCor);
       // console.log('yCOR : ' + yCor);
-      if(playing) {
+      if (playing) {
         snake.background(0);
-        for(let i=0;i<numSegments-1;i++) {
-          snake.line(xCor[i], yCor[i], xCor[i+1], yCor[i+1]);
+        for (let i = 0; i < numSegments - 1; i++) {
+          snake.line(xCor[i], yCor[i], xCor[i + 1], yCor[i + 1]);
         }
         updateSnakeCoordinates();
         checkGameStatus();
@@ -108,15 +111,27 @@ $(function() {
       }
     };
 
-    snake.keyPressed = function () {
-      switch(snake.keyCode) {
-      case snake.LEFT_ARROW : direction = 'left';
+    snake.keyPressed = function() {
+      switch (snake.keyCode) {
+      case snake.LEFT_ARROW:
+        if (direction != 'right') {
+          direction = 'left';
+        }
         break;
-      case snake.RIGHT_ARROW : direction = 'right';
+      case snake.RIGHT_ARROW:
+        if (direction != 'left') {
+          direction = 'right';
+        }
         break;
-      case snake.UP_ARROW : direction = 'up';
+      case snake.UP_ARROW:
+        if (direction != 'down') {
+          direction = 'up';
+        }
         break;
-      case snake.DOWN_ARROW : direction = 'down';
+      case snake.DOWN_ARROW:
+        if (direction != 'up') {
+          direction = 'down';
+        }
         break;
       }
     };
